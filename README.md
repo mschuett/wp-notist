1ce235541bc7aeab875e41c50bf00beb75da95f1
Martin Schütte <info@mschuette.name>
2019-04-12 11:27:27 +0200

    change to wp-notist

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# WordPress shortcode for Slideshare icon
+# WordPress shortcode for noti.st icon
 
 A _very_ simple WP plugin. Defines the shortcode
 
-    [slideshare url="http://www.slideshare.net/..."]
+    [noti.st url="https://noti.st/..."]
 
-and renders it as a linked Slideshare icon.
+and renders it as a linked noti.st icon.
+
+It is just a variant of https://github.com/mschuett/wp-slideshare
 
 I use it on: http://mschuette.name/wp/prasentationen/
---
+++ mschuett-notist.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: noti.st Shortcodes
+Plugin URI: http://mschuette.name/
+Description: Defines a shortcode for a noti.st logo and link
+Version: 0.1
+Author: Martin Schuette
+Author URI: http://mschuette.name/
+Licence: 2-clause BSD
+*/
+
+// Example:
+// [noti.st url="https://noti.st/mschuett/vr02LM/writing-ansible-modules"]
+function ms_notist_shortcode_handler( $atts ) {
+    extract(shortcode_atts( array(
+        'url' => 'http://www.noti.st/',
+    ), $atts));
+    $pluginurl = plugin_dir_url(__FILE__);
+
+    return '<a href="'.$url.'"><img style="height: .5em; padding: 0; margin: 0;" src="'.$pluginurl.'/noti.st_32px.png" alt="link to noti.st" /></a>';
+}
+
+if ( ! shortcode_exists( 'noti.st' ) ) {
+    add_shortcode( 'noti.st', 'ms_notist_shortcode_handler');
+}
+
